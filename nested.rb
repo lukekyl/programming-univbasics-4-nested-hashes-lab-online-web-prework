36fede870fb685c6a15e155f2b688ba13a244350
Kyle Luke <th.oldschool@gmail.com>
2019-07-27 16:27:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -70,10 +70,7 @@ def adding_matz
 
 	programmer_hash = 
  		{
- 		    :yukihiro_matsumoto => {
-          :known_for => "Ruby",
-          :languages => ["LISP", "C"]
-        },
+ 		    
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -89,7 +86,11 @@ def adding_matz
         
      }
 
-    programmer_hash[:yukihiro_matsumoto]
+    programmer_hash[:yukihiro_matsumoto] = {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
+        }
+          
 end
 
 def changing_alan
